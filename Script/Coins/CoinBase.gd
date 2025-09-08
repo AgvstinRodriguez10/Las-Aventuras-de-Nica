fad0c9a493e1a883231069cb7d743e0895ad3659
NicolasSaucedoDev <nicolas.saucedo.dev@gmail.com>
2025-09-08 10:09:31 -0300

    agregando animacion a monedas

--- Script/Coins/CoinBase.gd
+++ Script/Coins/CoinBase.gd
@@ -6,24 +6,33 @@ class_name CoinBase
 
 var distUp = 4
 var posInitial: Vector3
+var idleDistUp = 1.3
+var targetHeight 
 
 var target:Player
 
 enum STATE {
-	IDLE,
+	IDLE_DOWN,
+	IDLE_UP,
 	PULLING_UP,
 	PULL_IN,
 }
 
-var currentState := STATE.IDLE
+var currentState := STATE.IDLE_UP
 
 func _ready() -> void:
 	show()
 	posInitial = position
-	
+	targetHeight = posInitial.y + idleDistUp
 
 func _physics_process(delta: float) -> void:
 	match currentState:
+		STATE.IDLE_UP:
+			animationCoinsUp(delta)
+			animRotator(delta)
+		STATE.IDLE_DOWN:
+			animationCoinsDown(delta)
+			animRotator(delta)
 		STATE.PULLING_UP:
 			if distUp - 0.5 > posInitial.distance_to(position):
 				position = position.lerp(posInitial + Vector3(0,1,1) * distUp, 2 * delta)
@@ -35,8 +44,25 @@ func _physics_process(delta: float) -> void:
 			else:
 				byebye()
 
+func animationCoinsUp(delta:float):
+	#definido en el ready:
+	#targetHeight = posInitial.y + idleDistUp
+	if(position.y < targetHeight - 0.1):
+		position.y = lerpf(position.y, targetHeight, delta * 2.2)
+	else:
+		currentState = STATE.IDLE_DOWN
+
+func animationCoinsDown(delta:float):
+	if(position.y > posInitial.y + 0.05):
+		position.y = lerpf(position.y, posInitial.y, delta * 2)
+	else:
+		currentState = STATE.IDLE_UP
+
+func animRotator(delta:float):
+	rotate_y(lerp(0, 2, delta * 2))
+	
 func _on_body_entered(body: Player) -> void:
-	if body.name == "Nica" and currentState == STATE.IDLE:
+	if body.name == "Nica" and (currentState == STATE.IDLE_UP or currentState == STATE.IDLE_DOWN):
 		target = body
 		currentState = STATE.PULLING_UP
 
