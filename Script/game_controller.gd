d2b2d64374c531eff6865b5f31d477717c23b7ac
NicolasSaucedoDev <nicolas.saucedo.dev@gmail.com>
2025-09-19 13:36:18 -0300

    ajustes

--- Script/game_controller.gd
+++ Script/game_controller.gd
@@ -2,7 +2,8 @@ extends Node3D
 class_name GameController
 
 @onready var directional_light_3d: DirectionalLight3D = $"../DirectionalLight3D"
-@onready var camera_3d: Camera3D = $"../CameraFocus/Camera3D"
+#@onready var camera_3d: Camera3D = $"../CameraFocus/Camera3D"
+@onready var animation_player:AnimationPlayer = $"../CameraFocus/Camera3D/AnimationPlayer"
 
 func _ready() -> void:
 	pass
@@ -13,6 +14,7 @@ func _physics_process(delta: float) -> void:
 
 func time_controller(delta:float):
 	if directional_light_3d.light_energy > 0:
-		directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.03
-		#directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.4
-	
+		#directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.03
+		directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.8
+		if directional_light_3d.light_energy < 0.8:
+			animation_player.play("do_night")
