3641ef0ae501b03cb8bc11ead59280c4ceaa4d9a
NicolasSaucedoDev <nicolas.saucedo.dev@gmail.com>
2025-09-26 10:35:22 -0300

    arregalndo codigos para arrancar mas adelante en modo debug, limitando movimiento en estado movie

--- Script/game_controller.gd
+++ Script/game_controller.gd
@@ -4,19 +4,27 @@ class_name GameController
 @onready var directional_light_3d: DirectionalLight3D = $"../DirectionalLight3D"
 #@onready var camera_3d: Camera3D = $"../CameraFocus/Camera3D"
 @onready var animation_player:AnimationPlayer = $"../CameraFocus/Camera3D/AnimationPlayer"
-var isNight = false
+@export var debugDesdePlazaMitre: bool = false
+var isNight
 
 func _ready() -> void:
-	$"../Nica".position = Vector3(0,0,0.801)
+	isNight = false
+	if !debugDesdePlazaMitre:
+		$"../Nica".position = Vector3(0,0,0.801)
+	else:
+		$"../Nica".position = Vector3(0, 0, 546.0)
 
 func _physics_process(delta: float) -> void:
 	time_controller(delta)
-	
 
 func time_controller(delta:float):
 	if directional_light_3d.light_energy > 0:
-		directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.03
-		#directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.8
+		#si tiene el debug desdse la plaza, la oscuridad se acelera para ver efectos de noche
+		if !debugDesdePlazaMitre:
+			directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.03
+		else:
+			directional_light_3d.light_energy = directional_light_3d.light_energy - delta * 0.8
+		
 		if directional_light_3d.light_energy < 0.1 && !isNight:
 			animation_player.play("do_night")
 			isNight = true
