1db0b1f6c20a3366fef286292bba18f9f6a2c82e
AgvstinRodriguez10 <agvstinrodriguez10@gmail.com>
2025-10-02 10:05:01 -0300

    fixed

--- Script/Characters/nica.gd
+++ Script/Characters/nica.gd
@@ -59,11 +59,17 @@ func _physics_process(delta: float) -> void:
 	if $ShapeCastLeft.is_colliding():
 		collideL = true
 		$"TimerColl-L".start()
-	
+	else:
+		#$"TimerColl-L".stop()
+		#collideL = false
+		pass
 	if $ShapeCastRigth.is_colliding():
 		collideR = true
 		$"TimerColl -R".start()
-	
+	else:
+		#$"TimerColl -R".stop()
+		#collideR = false
+		pass
 	if currentState == STATES.RUN && !is_movie:
 		lateralController()
 		
