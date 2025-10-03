2ba8c434b4a91a2a08c19316f50a59f7da332516
AgvstinRodriguez10 <agvstinrodriguez10@gmail.com>
2025-10-03 11:23:03 -0300

    Testv2

--- Script/PathFollow.gd
+++ Script/PathFollow.gd
@@ -11,31 +11,30 @@ func _physics_process(delta: float) -> void:
 	progress += 0.08
 	
 	rayCastFloor.force_raycast_update()
-	rayCastLeft.force_raycast_update()
-	rayCastRigth.force_raycast_update()
 	
-	print(rayCastLeft.is_colliding())
+	print(colliderL)
 	
-	if Input.is_action_just_pressed("Derecha") and colliderR == false:
-		$Nica.position.x += 1
+	if $Nica/RayCastLeft.is_colliding():
+		colliderL = true
+		$Nica/TimerRayLeft.start()
+	else:
+		colliderL = false
+		
+	if $Nica/RayCastRigth.is_colliding():
+		colliderR = true
+		$Nica/TimerRayRigth.start()
+	else:
+		colliderR = false
+	
 	if Input.is_action_just_pressed("Izquierda") and colliderL == false:
 		$Nica.position.x -= 1
+	elif Input.is_action_just_pressed("Derecha") and colliderR == false:
+		$Nica.position.x += 1
 	
 	if rayCastFloor.is_colliding():
 		$Nica.position.y = 1
 	else:
 		$Nica.position.y = 0
-		
-	if rayCastLeft.is_colliding():
-		colliderL = true
-		$Nica/TimerRayLeft.start
-	else:
-		colliderL = false
-	if rayCastRigth.is_colliding():
-		colliderR = true
-		$Nica/TimerRayRigth.start()
-	else:
-		colliderR = false
 	
 
 
